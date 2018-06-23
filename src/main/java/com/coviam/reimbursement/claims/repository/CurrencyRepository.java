dc7d6061b081ffb0532a7a36f0c8ab40d072e406
Foram Shah <foram.shah@gdn-commerce.com>
2018-06-23 17:56:15 +0530

    Added RMBItemDto

---
+++ src/main/java/com/coviam/reimbursement/claims/repository/CurrencyRepository.java
@@ -0,0 +1,7 @@
+package com.coviam.reimbursement.claims.repository;
+
+/**
+ * @author Foram Shah on 23/06/18
+ */
+public class CurrencyRepository {
+}
---
+++ src/main/java/com/coviam/reimbursement/claims/service/impl/CurrencyServiceImpl.java
@@ -0,0 +1,7 @@
+package com.coviam.reimbursement.claims.service.impl;
+
+/**
+ * @author Foram Shah on 23/06/18
+ */
+public class CurrencyServiceImpl {
+}
