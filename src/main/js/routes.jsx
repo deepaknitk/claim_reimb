4215c9bf242ecefcdee4c16c9e2e1a935daf9ace
Jayant Rana <jayant.rana@gdn-commerce.com>
2018-06-24 12:38:56 +0530

    Profile, Login, Register

--- src/main/js/routes.jsx
+++ src/main/js/routes.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import RFQForm from './containers/RFQ/RFQForm';
 import NotFoundPage from './components/PageNotFound';
-import IndexPage from './containers/IndexPage';
+// import IndexPage from './containers/IndexPage';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Register from './components/Register';
 import SubmitSuccess from './components/SubmitSuccess';
 function buildRoutes(props) {
     return (
         <Switch>
-            <Route path="/" exact render={() => <IndexPage {...props}/>}/>
+            <Route path="/" exact render = {() => <Login property={props}/>}/>
             <Route path="/rfq" exact render={() => <RFQForm {...props}/>}/>
             <Route path="/submitSuccess" exact render = {()=> <SubmitSuccess/>}/>
+            <Route path="/profile" exact render = {() => <Profile user={props.user}/>}/>
+            <Route path="/register" component={Register}/>
             <Route path="*" component={NotFoundPage}/>
         </Switch>
     );
