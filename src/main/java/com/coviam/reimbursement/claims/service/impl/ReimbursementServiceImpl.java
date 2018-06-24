af91f42b832796e027e9c0a0e1f4dde7869391a1
Foram Shah <foram.shah@gdn-commerce.com>
2018-06-24 19:41:40 +0530

    Status code fix and findByRMBID

--- src/main/java/com/coviam/reimbursement/claims/service/impl/ReimbursementServiceImpl.java
+++ src/main/java/com/coviam/reimbursement/claims/service/impl/ReimbursementServiceImpl.java
@@ -34,7 +34,7 @@ import java.util.List;
         return rmb;
     }
 
-    @Override public ReimbursementItem findReimburesementByRmbItemId(Long rmbItemId) {
-        return null;
+    @Override public Reimbursement findReimburesementByRmbItemId(Long rmbId) {
+        return reimbursementRepository.findOne(rmbId);
     }
 }
