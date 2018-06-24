5a70ec16d2b82c85c81ed716d724478edbda2e85
Jayant Rana <jayant.rana@gdn-commerce.com>
2018-06-24 20:27:59 +0530

    Added

--- src/main/js/components/Login/index.js
+++ src/main/js/components/Login/index.js
@@ -3,17 +3,18 @@ import './styles.scss';
 
 const LOGIN = ({property}) => {
     return (
-        <div>
+        <div style={{ height: '100%' }}>
             {property.user === null ?
-                <div className="container-fluid" style={{backgroundColor: '#456C95', height: '620px'}}>
+                <div className="container-fluid" style={{backgroundColor: '#456C95', height: '100%'}}>
                     <div className="row" style={{height: '100%'}}>
-                        <div className="col justify-content-center align-self-center center-all">
+                        <div className="col justify-content-center align-self-center center-all reimbursement">
                             <h1 className="check"><span className="heading">₹</span>eimbursementBuzz</h1>
                             <a href="https://slack.com/oauth/authorize?client_id=304468210898.305541541543&scope=identity.avatar,identity.email,identity.basic"><img src="https://api.slack.com/img/sign_in_with_slack.png" /></a>
                         </div>
                     </div>
                 </div>
-                : property.history.push('/profile')}
+                : property.isRegisterActive === true ? property.history.push('/register') : property.history.push('/createClaim')
+            }
         </div>
     );
 };
